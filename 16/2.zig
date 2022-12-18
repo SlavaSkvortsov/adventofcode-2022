1bd6f2264ff5e9ff349fc6ceb43245ca4348e74e
SlavaSkvortsov <29122694+SlavaSkvortsov@users.noreply.github.com>
2022-12-18 23:15:27 +0100

    FUCK YEAH

--- 16/2.zig
+++ 16/2.zig
@@ -13,6 +13,11 @@ const Valve = struct {
     rate: u16,
 };
 
+const Elephant = struct {
+    valve: *Valve,
+    time: u8,
+};
+
 fn shortest_paths(from: Valve, valves: std.AutoHashMap([2]u8, Valve)) std.AutoHashMap([2]u8, u8) {
     var paths = std.AutoHashMap([2]u8, u8).init(allocator);
     paths.put(from.name, 0) catch unreachable;
@@ -39,38 +44,98 @@ fn shortest_paths(from: Valve, valves: std.AutoHashMap([2]u8, Valve)) std.AutoHa
     return paths;
 }
 
-// fn permutate(array: *std.ArrayList([2]u8), size: usize) void {
-fn permutate(array: *std.ArrayList([2]u8), size: usize) void {
-    if (size == 1) {
-        // print("{}\n", .{array});
-        return;
-    }
-
-    var i: usize = 0;
-    while (i < size) : (i += 1) {
-        permutate(array, size - 1);
-        var elem = array.swapRemove(if (size % 2 == 1) 0 else i);
-        array.append(elem) catch unreachable;
+fn print_depth(depth: u8) void {
+    var i: u8 = 0;
+    while (i < depth) : (i += 1) {
+        print("  ", .{});
     }
 }
 
-fn brute_force(current_valve: Valve, closed_valves: std.AutoHashMap([2]u8, Valve), time_left: u8) u32 {
+fn brute_force(elephants: [2]Elephant, closed_valves: std.AutoHashMap([2]u8, Valve), time_left: u8, depth: u8) u32 {
     var max_result: u32 = 0;
+    var count: u32 = 0;
 
-    var closed_valves_iter = closed_valves.iterator();
-    while (closed_valves_iter.next()) |data| {
-        if (current_valve.paths.get(data.key_ptr.*)) |path| {
-            if (path < time_left) {
-                var new_closed_valves = closed_valves.clone() catch unreachable;
-                _ = new_closed_valves.remove(data.key_ptr.*);
-                const result = brute_force(data.value_ptr.*, new_closed_valves, time_left - path - 1);
-                if (result > max_result) {
-                    max_result = result;
+    for (elephants) |elephant, i| {
+        if (elephant.time > 0) continue;
+
+        var closed_valves_iter = closed_valves.iterator();
+        while (closed_valves_iter.next()) |data| {
+            if (depth == 0 ) {
+                print("count: {}\n", .{count});
+                count += 1;
+            }
+            if (elephant.valve.paths.get(data.key_ptr.*)) |path| {
+                if (path < time_left) {
+                    var new_closed_valves = closed_valves.clone() catch unreachable;
+                    defer new_closed_valves.deinit();
+                    _ = new_closed_valves.remove(data.key_ptr.*);
+
+
+                    var new_elephants = [2]Elephant{
+                        Elephant{
+                            .valve = if (i == 0) data.value_ptr else elephants[0].valve,
+                            .time = if (i == 0) path + 1 else elephants[0].time,
+                        },
+                        Elephant{
+                            .valve = if (i == 1) data.value_ptr else elephants[1].valve,
+                            .time = if (i == 1) path + 1 else elephants[1].time,
+                        },
+                    };
+
+                    var min_time = if (new_elephants[0].time < new_elephants[1].time) new_elephants[0].time else new_elephants[1].time;
+
+                    for (new_elephants) |*new_elephant| {
+                        new_elephant.*.time -= min_time;
+                    }
+                    // print_depth(depth);
+                    // print("Elephant {d} is opening valve {s} wasting {}\n", .{i, data.key_ptr.*, path + 1});
+                    //
+                    // print_depth(depth);
+                    // print("Time left: {}\n", .{time_left - min_time});
+                    //
+                    // print_depth(depth);
+                    // print("New time: {}, {}\n", .{new_elephants[0].time, new_elephants[1].time});
+                    //
+                    // print_depth(depth);
+                    // print("New valve: {s}, {s}\n", .{new_elephants[0].valve.name, new_elephants[1].valve.name});
+                    //
+                    // print_depth(depth);
+                    // print("Closed valves: {}\n", .{new_closed_valves.count()});
+
+                    // print_depth(depth);
+                    // print("Max result: {}\n", .{max_result});
+
+                    // print_depth(depth);
+                    // print("\n", .{});
+                    // var threads: std.ArrayList(*std.Thread) = std.ArrayList(*std.Thread).init(allocator);
+                    // var t: usize = 0;
+                    //
+                    // while (t < num_threads) : (t += 1) {
+                    //     // Start a new thread and pass the context to the worker thread.
+                    //     try threads.append(try std.Thread.spawn(worker_ctx, workerFunction));
+                    // }
+                    //
+                    // // Wait for all threads to finish.
+                    // for (threads.items) |thread| {
+                    //     thread.wait();
+                    // }
+
+                    const result = brute_force(
+                        new_elephants,
+                        new_closed_valves,
+                        time_left - min_time,
+                        depth + 1,
+                    ) + (data.value_ptr.*.rate * (time_left - path - 1));
+                    if (result > max_result) {
+                        max_result = result;
+                    }
                 }
             }
         }
     }
-    return max_result + time_left * current_valve.rate;
+    // print_depth(if (depth > 0) depth - 1 else 0);
+    // print("Max result: {}\n", .{max_result});
+    return max_result;
 }
 
 pub fn main() !void {
@@ -134,11 +199,23 @@ pub fn main() !void {
         }
     }
 
-    permutate(&closed_values_list, closed_values_list.items.len);
-    // const result = brute_force(
-    //     valves.get([_]u8{ 'A', 'A' }).?,
-    //     closed_valves,
-    //     26,
-    // );
-    // print("Result = {}\n", .{result});
+
+
+    var start_valve = valves.get([_]u8{ 'A', 'A' }).?;
+    const result = brute_force(
+        [2]Elephant{
+            Elephant{
+                .valve = &start_valve,
+                .time = 0,
+            },
+            Elephant{
+                .valve = &start_valve,
+                .time = 0,
+            },
+        },
+        closed_valves,
+        26,
+        0,
+    );
+    print("Result = {}\n", .{result});
 }
